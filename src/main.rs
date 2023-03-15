d55f5ce765977d7c725b8aa4cf7bc7206d908626
mreleftheros <eleftheros_all@protonmail.com>
2023-03-15 20:32:19 +0200

    add read file method and search

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, error, process};
+use std::{env, error, process, fs};
 
 #[derive(Debug)]
 struct Env<'a> {
@@ -19,15 +19,27 @@ impl<'a> Env<'a> {
             }
         )
     }
+
+    fn search(&self, file: &str) {
+        let q = self.query;
+        let lines = file.lines();
+
+        for line in lines {
+            println!("{}", line);
+        }
+    }
 }
 
 fn run(args: &[String]) -> Result<(), Box<dyn error::Error>> {
     let env = Env::from_args(&args)?;
+    let file = fs::read_to_string(&env.path)?;
+    env.search(&file);
     Ok(())
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
     if let Err(err) = run(&args) {
         eprintln!("Error: {}", err);
         process::exit(1);
