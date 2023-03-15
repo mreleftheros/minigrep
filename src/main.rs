230bf894e361b8726ae81ce7a80d9c21b1cba3cb
mreleftheros <eleftheros_all@protonmail.com>
2023-03-15 20:09:19 +0200

    add from_args method in Env

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
 use std::env;
 
+#[derive(Debug)]
 struct Env<'a> {
     query: &'a str,
     path: &'a str
 }
 
+impl<'a> Env<'a> {
+    fn from_args(args: &'a[String]) -> Result<Self, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+
+        Ok(
+            Self {
+                query: &args[1],
+                path: &args[2]
+            }
+        )
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let env = Env::from_args(&args);
 
-    println!("{:?}", args);
+    println!("{:?}", env);
 }
